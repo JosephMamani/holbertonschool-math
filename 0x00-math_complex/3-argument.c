db3ae2bd0f391bb44a8c261d0c8a48c9b83dfa9d
JosephMamani <jmmollenedo@gmail.com>
2021-11-13 05:22:03 +0000

    function that returns the argument of a given complex number.

---
+++ 0x00-math_complex/3-argument.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * argument - gets the argument of a given complex number
+ * @c: complex number
+ * Return: the argument of a given complex number.
+ */
+double argument(complex c)
+{
+	if (c.re != 0)
+		return (atan(c.im / c.re));
+	else
+		return (0);
+}
